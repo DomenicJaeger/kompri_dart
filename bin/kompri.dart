50d8b32c23c975f605c5a632845470ed2e1d17f7
DomenicJaeger <domenic_jaeger@teilnehmer.cdemy.de>
2023-09-29 08:50:34 +0200

    durchkommentiert

--- bin/kompri.dart
+++ bin/kompri.dart
@@ -14,7 +14,9 @@ void main() {
 }
 
 bool validateInput(String input) {
-  // Überprüfen, ob der String nur Buchstaben enthält
+  //Überprüfen, ob der String nur Buchstaben enthält
+  //Buchstaben werden durch Leerzeichen ersetzt, sind alle Zeichen Buchstaben,
+  //wird ein leerer String mit einem leeren String verglichen, bool ist true
   return input.replaceAll(RegExp(r'[a-zA-Z]'), '') == '';
 }
 
@@ -24,15 +26,18 @@ String compress(String input) {
 
   for (int i = 0; i < input.length; i++) {
     if (i < input.length - 1 && input[i] == input[i + 1]) {
+      // < input.length -1 da er mit dem folgenden Zeichen vergleicht, und das allerletzte Zeichen nicht vergleichen kann
       // Wenn das aktuelle Zeichen dem nächsten Zeichen entspricht, erhöhen wir den Zähler
       count++;
     } else {
       if (count >= 3) {
+        //Wenn der counter 3 oder größer ist, wird das Zeichen mit dem Counter-Wert zusammengefügt
+        //anderenfalls wird das Zeichen so oft dargestellt wie der Counter-Wert hoch ist
         compressed += input[i] + count.toString();
       } else {
         compressed += input[i] * count;
       }
-      count = 1; // Reset des Zählers
+      count = 1; // Reset des Zählers nach der Schleife
     }
   }
   return compressed;
